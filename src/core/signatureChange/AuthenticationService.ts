24397febd007eb35544435fa1bdc60fd625e5ded
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2025-01-31 19:19:27 +0100

    ParallelChange: create Id domain type

--- src/core/signatureChange/AuthenticationService.ts
+++ src/core/signatureChange/AuthenticationService.ts
@@ -3,3 +3,14 @@ export class AuthenticationService {
 		return (id == 12345);
 	}
 }
+
+export class Id {
+	private constructor(private readonly id: number) {
+		this.id = id;
+	}
+	static create(id: number): Id {
+		if (id <= 0 )
+			throw Error(`id ${id} not valid`);
+		return new Id(id);
+	}
+}
