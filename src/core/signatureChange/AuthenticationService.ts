c9a18b4224f319acdc948e9986f433e73ef315a1
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2025-01-31 19:22:19 +0100

    ParallelChange: Expand phase

--- src/core/signatureChange/AuthenticationService.ts
+++ src/core/signatureChange/AuthenticationService.ts
@@ -2,6 +2,9 @@ export class AuthenticationService {
 	isAuthenticated(id: number): boolean {
 		return (id == 12345);
 	}
+	isAuthenticatedNew(id: Id): boolean {
+		return (id.equals(Id.create(12345)));
+	}
 }
 
 export class Id {
@@ -12,5 +15,9 @@ export class Id {
 		if (id <= 0 )
 			throw Error(`id ${id} not valid`);
 		return new Id(id);
+	}
+	
+	public equals(other: Id): boolean {
+		return other.id === this.id;
 	}
 }
